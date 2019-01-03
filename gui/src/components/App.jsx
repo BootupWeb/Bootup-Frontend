1c2a5e4a40f7b02463b75eae3ceb6d7ef1fde564
omar jandali <omarjandali@omars-MacBook-Pro.local>
2019-01-03 14:55:12 -0800

    created a connection between local react application and django backend RESTful API

--- gui/src/components/App.jsx
+++ gui/src/components/App.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      details: {},
+    }
+    Axios.defaults.baseURL = "http://127.0.0.1:8000/api/"
+  }
+
+  componentWillMount(){
+    this.fetchDetails();
+  }
+
+  fetchDetails(){
+    Axios.get('campaign/details/')
+      .then((data) => {
+        console.log(data.data)
+      })
+      .catch((err) => {
+        console.log('fetching details error: ' + err)
+      })
+  }
+
   render() {
     return (
       <div className="App">
