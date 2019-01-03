f478e8f7812b91358bebeb5736895ebcf619921b
omar jandali <omarjandali@omars-mbp.lan>
2019-01-03 10:52:38 -0800

    added webpack file to the folder

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const { env } = require('./env/.env');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './client/src/'),
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js[x]?/,
+        exclude: /node_modules/,
+        options: {
+          presets: [['env', { modules: false }], 'react'],
+          env: {
+            test: {
+              presets: [['env'], 'react'],
+            },
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[hash:base64]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.HOSTNAME': JSON.stringify(env.HOSTNAME),
+      'process.env.PORT': JSON.stringify(env.PORT),
+    }),
+  ],
+};
