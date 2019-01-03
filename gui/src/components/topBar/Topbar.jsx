b41fb834cea1ebaee1d204abf5f22a1f0541ed2c
omar jandali <omarjandali@omars-MacBook-Pro.local>
2019-01-03 15:02:01 -0800

    added the main top bar component that will consist of the loog, search, notifications, settings, logout, and activity

---
+++ gui/src/components/topBar/Topbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Topbar extends Component {
+    contructor(props){
+        super(props);
+        this.setState({
+            search: ''
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
