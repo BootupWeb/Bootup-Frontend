5cfd0c5f9519ba7022e15186884a3f3b63e8a8e6
omar jandali <omarjandali@omars-mbp.lan>
2019-01-05 14:39:28 -0800

    I have added the initial version of the campaign tab in the side bar

---
+++ gui/src/components/sideBar/CampaignTab.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function CampaignTab(props){
+
+    return(
+        <div className="campaign-tab-container">
+            <div className="campaign-content">
+                <div className="featured-image-container">
+                    <img className="featured-image" src="http://via.placeholder.com/120x240" alt=""/>
+                </div>
+                <div className="campaign-summary">
+                    <span className="campagin-name">Name</span>
+                    <span className="campaign-category">category</span>
+                    <span className="campaign-slug">slug</span>
+                </div>
+                <div className="asking-container">
+                    <div classname="asking-content">
+                        <span className="asking-equity">equity</span>
+                        <span className="asking-amount">amount</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
