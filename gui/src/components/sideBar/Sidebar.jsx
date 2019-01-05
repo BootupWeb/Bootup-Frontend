8569cb7ce054f44f4bf4d8d06a324c9967eec869
omar jandali <omarjandali@omars-mbp.lan>
2019-01-05 15:44:18 -0800

    i have added the legal component to the main bootup app

--- gui/src/components/sideBar/Sidebar.jsx
+++ gui/src/components/sideBar/Sidebar.jsx
@@ -11,7 +11,7 @@ export default function SideBar(props){
             <UserTab />
             <CampaignTab />
             <CategoryTab />
-            {/*<Legal /> */}
+            <Legal /> 
         </div>
     )
 
