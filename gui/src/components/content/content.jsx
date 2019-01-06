5cbe0bcfaa244063169bb878575a757b0f4a9754
omar jandali <omarjandali@omars-mbp.lan>
2019-01-05 17:58:40 -0800

    created all of the required files from the content details sections

---
+++ gui/src/components/content/content.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Content extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            details: '',
+            slug: '',
+        }
+    }
+
+    render(){
+
+        return(
+            <div className="details-container">
+                
+            </div>
+        )
+
+    }
+
+}
