b1be1d190631817e404fbbc4cc085022be8e7b1f
omar jandali <omarjandali@omars-mbp.lan>
2019-01-05 19:01:05 -0800

    created the founder data for the details section

---
+++ gui/src/components/content/detailsTab/founderDetails.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function FounderDetails(props){
+
+    return(
+        <div classname="founder-details-container">
+            <div className="founder-list-content">
+                <ul className="founder-list">
+                    <li className="founder-item">
+                        <span className="founder-name">Founder</span><br/>
+                        <span className="founder-position">Position</span><br/>
+                    </li> <br/>
+                    <li className="founder-item">
+                        <span className="founder-name">Founder</span><br/>
+                        <span className="founder-position">Position</span><br/>
+                    </li> <br/>
+                    <li className="founder-item">
+                        <span className="founder-name">Founder</span><br/>
+                        <span className="founder-position">Position</span><br/>
+                    </li> <br/>
+                </ul>
+            </div>
+        </div>
+    )
+
+}
