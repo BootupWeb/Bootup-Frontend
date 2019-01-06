b0f6e5da8bec7128479f341f83f566c9a78db5d3
omar jandali <omarjandali@omars-mbp.lan>
2019-01-05 18:54:37 -0800

    added the initial placeholder for quick details

---
+++ gui/src/components/content/detailsTab/quickDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function quickDetails(props){
+
+    return(
+        <div className="quick-details-container">
+            <span className="quick-slug">Slug</span> <br/>
+            <span className="quick-summary">Summary</span> <br/>
+        </div>
+    )
+
+}
