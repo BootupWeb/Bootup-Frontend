e5d0d302588f66e41ac42424e32cfba98cc1474f
omar jandali <omarjandali@omars-MacBook-Pro.local>
2019-01-07 12:07:41 -0800

    added the update comppnent and the initial placeholder for the update items

---
+++ gui/src/components/content/main/update.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UpdateListItem from './updateListItem';
+
+export default function Update(props){
+
+    return(
+        <div className="update-container">
+            <div className="update-content">
+                <UpdateListItem />
+            </div>
+        </div>
+    )
+
+}
---
+++ gui/src/components/content/main/updateListItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function UpdateListItem(props){
+
+    return(
+        <div className="update-list-item-container">
+            <div className="update-item-content">
+                <span classname="update-subject">Subject</span>
+                <span classname="update-message">message</span>
+                <span classname="update-impact">impact</span>
+                <span classname="update-founder">founder</span>
+                <span classname="update-date-time">date/time</span>
+            </div> 
+            <div className="update-item-content">
+                <span classname="update-subject">Subject</span>
+                <span classname="update-message">message</span>
+                <span classname="update-impact">impact</span>
+                <span classname="update-founder">founder</span>
+                <span classname="update-date-time">date/time</span>
+            </div> 
+            <div className="update-item-content">
+                <span classname="update-subject">Subject</span>
+                <span classname="update-message">message</span>
+                <span classname="update-impact">impact</span>
+                <span classname="update-founder">founder</span>
+                <span classname="update-date-time">date/time</span>
+            </div> 
+        </div>
+    )
+
+}
